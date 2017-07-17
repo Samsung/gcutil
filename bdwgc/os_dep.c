af510c72eec3fc3106dde7197cf7bfd914d968d0
변무홍/Web Platform Lab(S/W센터)/Senior Engineer/삼성전자 <mh.byun@samsung.com>
2017-07-17 09:53:06 +0900

    Rename macro for better understanding (TIZEN_WEARABLE -> TIZEN) (#3)
    Signed-off-by: MuHong Byun <mh.byun@samsung.com>

--- bdwgc/os_dep.c
+++ bdwgc/os_dep.c
@@ -423,7 +423,7 @@ GC_INNER char * GC_get_maps(void)
 
   GC_INNER void GC_init_linux_data_start(void)
   {
-#if (defined(TIZEN_WEARABLE))
+#if (defined(TIZEN))
     GC_data_start = DATAEND;
     return;
 #endif
